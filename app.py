9d11a012a88a42eb283a177ef26d0b6c9a067533
Ubuntu <ubuntu@ip-172-31-28-221.ec2.internal>
2021-03-14 17:36:20 +0000

    aggiunto metodo post

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-@app.route('/ask', METHOD=['POST'])
+
+@app.route('/ask', methods=["GET", "POST"])
 def ask():
 
     data = request.get_json()
