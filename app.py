d32081e55db655c45f031d5190d842559b9f8cdd
Visioweb <58529746+Visioweb@users.noreply.github.com>
2021-03-24 16:52:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,27 +22,32 @@ def login():
         username = request.form['username']
         password = request.form['password']
         
-        msg = username + password
         
-        '''
+        
+       
         db = db_connect()
         cursor = db.cursor()
 
         cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
+
         # If account exists in accounts table in out database
         if account:
+            
+            msg = account
+            '''
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
             # Redirect to home page
             return 'Logged in successfully!'
+            '''
         else:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
-        '''
+
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
 
