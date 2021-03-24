af11f7a6ec5f1959ba04e023656c10ba23a900e1
Visioweb <58529746+Visioweb@users.noreply.github.com>
2021-03-24 16:06:54 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -244,7 +244,7 @@ class Chatbot:
 
         f.close()
         
-    def _save_conv_db(question, response, intent, y_proba_max, errore, errors=False):
+    def _save_conv_db(self, question, answer, intent, proba, errore, error=False):
         db = db_connect()
         cursor = db.cursor()
         formatted_date = dt.now().strftime("%Y/%m/%d %H:%M:%S")  
