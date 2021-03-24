9ec86fe4bf2dd2a1ced246d9f37267d8bf3748ac
Visioweb <58529746+Visioweb@users.noreply.github.com>
2021-03-24 17:09:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,10 +50,12 @@ def login():
 # http://localhost:5000/python/logout - this will be the logout page
 @app.route('/login/logout')
 def logout():
+    '''
     # Remove session data, this will log the user out
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('username', None)
+   '''
    # Redirect to login page
    return redirect(url_for('login'))
 
