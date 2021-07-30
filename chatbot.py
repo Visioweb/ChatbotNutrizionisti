97bf1dd662e6f7f6a9dd26188d634e2fef7730ff
Ufficio <info@visioweb.it>
2021-07-30 10:38:03 +0200

    cambio corpus

--- chatbot.py
+++ chatbot.py
@@ -294,7 +294,7 @@ if __name__ == '__main__':
     from actions import *
 
     chatbot = Chatbot(sensitivity=.4)
-    chatbot.train("corpus_with_context.json", verbose=False)
+    chatbot.train("andrea_corpus.json", verbose=False)
     chatbot.load()
     answer = chatbot.ask("come va?", return_proba=True)
     print(answer)
