8a337b5a110cb7e7c37a9560088339c6970431ab
Ufficio <info@visioweb.it>
2021-08-04 12:22:14 +0200

    intents uniti

--- telegram_bot.py
+++ telegram_bot.py
@@ -19,8 +19,8 @@ def bot_ask(update: Update, context: CallbackContext):
 
 def start(update: Update, context: CallbackContext):
     msgs = [
-        'Salve sono Andrea, il virtual Concierge di Baia Marticana.\n Posso darle informazioni sulla struttura o turistiche o prendere segnalazioni relative alla camera. Come posso aiutarla?',
-        'Salve sono Andrea, il virtual Concierge, le do il benvenuto in Baia Marticana.\n Può chiedermi informazioni turistiche o sulla struttura e fare segnalazioni relative alla camera. Come posso essere utile?'
+        'Salve sono Andrea, il virtual Concierge di Baia Marticana.\nPosso darle informazioni sulla struttura o turistiche o prendere segnalazioni relative alla camera. Come posso aiutarla?',
+        'Salve sono Andrea, il virtual Concierge, le do il benvenuto in Baia Marticana.\nPuò chiedermi informazioni turistiche o sulla struttura e fare segnalazioni relative alla camera. Come posso essere utile?'
     ]
     update.effective_message.reply_text(
         choice(msgs)
