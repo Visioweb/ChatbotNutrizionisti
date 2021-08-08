13bf81b1c98fa94d866c9607d0d575ee53515d41
Ufficio <info@visioweb.it>
2021-08-08 16:19:49 +0200

    sleep second

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,24 +1,37 @@
 from telegram.ext import Updater, CallbackContext, Filters, Dispatcher, MessageHandler, CommandHandler
-from telegram import Update
+from telegram import Update, ChatAction
 from random import choice
-
+from time import sleep
 from chatbot import Chatbot
 
 
+def typing(secs: int = 1):
+    def decorator(func):
+        def wrapper(update: Update, context: CallbackContext):
+            context.bot.send_chat_action(update.effective_chat.id, ChatAction.TYPING)
+            sleep(secs)
+            func(update, context)
+
+        return wrapper
+
+    return decorator
+
+
+@typing()
 def bot_ask(update: Update, context: CallbackContext):
     print('Messaggio ricevuto:', update.effective_message.text)
     print('Context attuale:', context.chat_data.get('context'))
 
-
-    chatbot = Chatbot(sensitivity = 0.4)
+    chatbot = Chatbot(sensitivity=0.4)
     chatbot.load()
     response, new_context, _ = chatbot.ask(update.effective_message.text,
-                                           current_context = context.chat_data.get('context'),
-                                           return_proba = True)
+                                           current_context=context.chat_data.get('context'),
+                                           return_proba=True)
     context.chat_data['context'] = new_context
     update.effective_message.reply_text(response)
 
 
+@typing()
 def start(update: Update, context: CallbackContext):
     msgs = [
         'Salve sono Andrea, il virtual Concierge di Baia Marticana.\nPosso darle informazioni sulla struttura, info turistiche o prendere segnalazioni relative alla camera. Come posso aiutarla?',
@@ -28,6 +41,8 @@ def start(update: Update, context: CallbackContext):
         choice(msgs)
     )
 
+
+@typing()
 def nointent(update: Update, context: CallbackContext):
     messgs = [
         'ok, qualunque cosa non esiti a contattarmi.',
@@ -38,12 +53,15 @@ def nointent(update: Update, context: CallbackContext):
         choice(messgs)
     )
 
+
 updater = Updater('1740966801:AAEdTnF2FQETbN8SO8i1Y67kO0QgYXPwc5k')
 dispatcher: Dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start, Filters.chat_type.private))
 
-dispatcher.add_handler(MessageHandler(Filters.chat_type.private&Filters.text(["no", "No", "no grazie", "No Grazie", "No grazie", "non mi serve altro"]), nointent))
+dispatcher.add_handler(MessageHandler(Filters.chat_type.private & Filters.text(["no", "No", "no grazie", "No Grazie",
+                                                                                "No grazie", "non mi serve altro"]),
+                                      nointent))
 
 dispatcher.add_handler(MessageHandler(Filters.text & Filters.chat_type.private, bot_ask))
 
